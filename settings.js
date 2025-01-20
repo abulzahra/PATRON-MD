887c0b2d2d36474175a9522b83207cf304ebde51
abulzahra <wilbertmombo77@gmail.com>
2025-01-20 15:32:28 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "MPESA"
 
-global.banknumber = "8133729715"
+global.banknumber = "756551505"
 
-global.bankowner = "FADARE"
+global.bankowner = "SIMBA"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = true //auto recording (true to on, false to off)
